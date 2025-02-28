97fe009496541a71f7e052c4e76c45a8b5c2c50e
eggnog <daniel@eggnog.dev>
2025-02-28 10:51:44 +0200

    create plasma6 module

---
+++ modules/nixos/desktop-environments/plasma6.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.system.desktopEnvironment.plasma5;
+in
+{
+  options = {
+    system.desktopEnvironment.plasma5 = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Plasma6 Desktop Environment";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.xserver.enable = true;
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+  };
+}
