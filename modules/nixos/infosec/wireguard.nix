51907c481630662bc469a88e86bab4040b7355d3
eggnog <daniel@eggnog.dev>
2025-03-14 10:29:57 +0200

    create wireguard module

---
+++ modules/nixos/infosec/wireguard.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.infosec.wireguard;
+in
+{
+  options = {
+    programs.infosec.wireguard = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Wireguard";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      wireguard-tools
+      wireguard-ui
+    ];
+  };
+}
