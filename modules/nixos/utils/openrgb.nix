fa133811f8eec2745ef7b18015eb87821bb2d61a
eggnog <daniel@eggnog.dev>
2025-06-11 17:19:19 +0300

    update openrgb module

--- modules/nixos/utils/openrgb.nix
+++ modules/nixos/utils/openrgb.nix
@@ -2,6 +2,9 @@
 
 let
   cfg = config.programs.utils.openrgb;
+  openrgb-rules = builtins.fetchurl {
+    url = "https://gitlab.com/CalcProgrammer1/OpenRGB/-/raw/master/60-openrgb.rules";
+  };
 in
 {
   options = {
@@ -15,6 +18,9 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
+    services.udev.extraRules =  builtins.readFile openrgb-rules;
+
     environment.systemPackages = with pkgs; [
       openrgb
     ];
