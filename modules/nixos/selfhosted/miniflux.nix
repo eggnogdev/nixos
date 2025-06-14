3251b190c9b04adc308c9bc73234b0ffefdb4cfb
eggnog <daniel@eggnog.dev>
2025-06-14 20:16:43 +0300

    setup miniflux instance

---
+++ modules/nixos/selfhosted/miniflux.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.selfhosted.miniflux;
+in
+{
+  options = {
+    programs.selfhosted.miniflux = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Enable Miniflux";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.miniflux = {
+      enable = true;
+
+      adminCredentialsFile = "/etc/miniflux/admin";
+
+      config = {
+        PORT = 27159;
+      };
+    };
+  };
+}
