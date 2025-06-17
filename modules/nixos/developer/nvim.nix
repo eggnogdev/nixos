e4ae4e85c7ce47f4157b6787d076c10e6a86fc21
eggnog <daniel@eggnog.dev>
2025-06-17 11:37:12 +0300

    update nvim config

--- modules/nixos/developer/nvim.nix
+++ modules/nixos/developer/nvim.nix
@@ -20,8 +20,27 @@ in
       settings.vim = {
         theme = {
           enable = true;
-          name = "dracula";
-          style = "dark";
+          name = "base16";
+
+          # https://github.com/ntpeters/base16-materialtheme-scheme/blob/master/material.yaml
+          base16-colors = {
+            base00 = "#263238";
+            base01 = "#2E3C43";
+            base02 = "#314549";
+            base03 = "#546E7A";
+            base04 = "#B2CCD6";
+            base05 = "#EEFFFF";
+            base06 = "#EEFFFF";
+            base07 = "#FFFFFF";
+            base08 = "#F07178";
+            base09 = "#F78C6C";
+            base0A = "#FFCB6B";
+            base0B = "#C3E88D";
+            base0C = "#89DDFF";
+            base0D = "#82AAFF";
+            base0E = "#C792EA";
+            base0F = "#FF5370";
+          };
         };
 
         lsp.enable = true;
@@ -34,6 +53,11 @@ in
         statusline.lualine.enable = true;
         telescope.enable = true;
         autocomplete.nvim-cmp.enable = true;
+
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
       };
     };
   };
